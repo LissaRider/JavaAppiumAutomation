b3ff5fcc0e16ee358a2d39d8dc890d09c78e7a9c
Vasilisa_Smirnova <vssmirnova@vtb.ru>
2021-04-08 21:48:25 +0300

    Реализованы поиск элемента по тексту и нажатие по найденному элементу
    Lesson: 01. Первый тест: find_element, click
    Session: III. Первые тесты на Android

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -2,6 +2,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -9,36 +10,36 @@ import java.net.URL;
 
 public class FirstTest {
 
+  private AndroidDriver<?> driver;
 
-    private AndroidDriver<?> driver;
+  @Before
+  public void SetUp() throws Exception {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("platformName", "Android");
+    capabilities.setCapability("deviceName", "AndroidTestDevice");
+    capabilities.setCapability("platformVersion", "8.0");
+    capabilities.setCapability("automationName", "Appium");
+    capabilities.setCapability("appPackage", "org.wikipedia");
+    capabilities.setCapability("appActivity", ".main.MainActivity");
 
-    @Before
-    public void SetUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-
-        String oldWikiPath = "./apks/org.wikipedia.old.apk";
-        capabilities.setCapability("app", new File(oldWikiPath).getCanonicalPath());
+    String oldWikiPath = "./apks/org.wikipedia.old.apk";
+    capabilities.setCapability("app", new File(oldWikiPath).getCanonicalPath());
 /*        String stableWikiPath = "./apks/org.wikipedia.stable.apk";
         capabilities.setCapability("app", new File(stableWikiPath).getCanonicalPath());*/
 /*        String betaWikiPath = "./apks/org.wikipedia.beta.apk";
         capabilities.setCapability("app", new File(betaWikiPath).getCanonicalPath());*/
 
-        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
+    driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+  }
 
-    @After
-    public void TearDown() {
-        driver.quit();
-    }
+  @After
+  public void TearDown() {
+    driver.quit();
+  }
 
-    @Test
-    public void firstTest() {
-        System.out.println("First test run");
-    }
+  @Test
+  public void firstTest() {
+    WebElement element = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+    element.click();
+  }
 }
