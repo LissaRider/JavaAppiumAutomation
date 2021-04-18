04c44e93d4c73fa2635053983aee5a242a751cb6
Vasilisa_Smirnova <vssmirnova@vtb.ru>
2021-04-18 15:52:37 +0300

    Реализован тест с действием "swipe"
    Lesson: 01. Swipe: start_x, Touch action, dimensions
    Session: IV. Продвинутые тесты на Android

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,11 @@
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -203,6 +205,38 @@ public class FirstTest {
   }
 
   @Test
+  public void testSwipeArticle() {
+
+    waitForElementAndClick(
+            By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+            "Внимание! Элемент 'Search Wikipedia' не найден.",
+            5
+    );
+
+    waitForElementAndSendKeys(
+            By.xpath("//*[contains(@text,'Search…')]"),
+            "Java",
+            "Внимание! Поле ввода текста для поиска не найдено.",
+            5
+    );
+
+    waitForElementAndClick(
+            By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
+                    "//*[@text='Object-oriented programming language']"),
+            "Внимание! Текст 'Object-oriented programming language' не найден.",
+            5
+    );
+
+    waitForElementPresent(
+            By.id("org.wikipedia:id/view_page_title_text"),
+            "Внимание! Заголовок статьи не найден.",
+            15
+    );
+
+    swipeUp(2000);
+  }
+
+  @Test
   public void testSearchResults() {
 
     final String searchValue = "JAVA";
@@ -287,5 +321,14 @@ public class FirstTest {
     WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
     wait.withMessage("\n  " + errorMessage + "\n");
     return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+  }
+
+  protected void swipeUp(int timeOfSwipe) {
+    TouchAction action = new TouchAction(driver);
+    Dimension size = driver.manage().window().getSize();
+    int x = size.width / 2;
+    int startY = (int) (size.height * 0.8);
+    int endY = (int) (size.height * 0.2);
+    action.press(x, startY).waitAction(timeOfSwipe).moveTo(x, endY).release().perform();
   }
 }
