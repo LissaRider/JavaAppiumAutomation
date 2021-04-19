57cca57402b4038f216f8d354c197ea8f30e389e
Vasilisa_Smirnova <vssmirnova@vtb.ru>
2021-04-19 18:53:58 +0300

    Реализован тест testActionsWithArticlesInFolder(), который: 1. Сохраняет две статьи в одну папку 2. Удаляет одну из статей 3. Убеждается, что вторая осталась 4. Переходит в неё и убеждается, что title совпадает
    Homework: Ex5: Тест: сохранение двух статей
    Session: IV. Продвинутые тесты на Android

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,10 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -276,7 +273,7 @@ public class FirstTest {
   }
 
   @Test
-  public void saveFirstArticleToMyList() {
+  public void testSaveFirstArticleToMyList() {
 
     waitForElementAndClick(
             By.xpath("//*[contains(@text,'Search Wikipedia')]"),
@@ -552,6 +549,96 @@ public class FirstTest {
     );
   }
 
+  @Test
+  public void testActionsWithArticlesInFolder() {
+
+    String searchWotLine = "World of Tanks";
+
+    searchFor(searchWotLine);
+
+    openArticle(searchWotLine);
+
+    selectOption("Add to reading list");
+
+    String folderName = "Games";
+
+    createAndAddArticleTo(folderName);
+
+    String searchWowLine = "World of Warcraft";
+
+    searchFor(searchWowLine);
+
+    openArticle(searchWowLine);
+
+    selectOption("Add to reading list");
+
+    addArticleTo(folderName);
+
+    closeArticle();
+
+    openMyLists();
+
+    openFolder(folderName);
+
+    By searchResultLocator = By.id("org.wikipedia:id/page_list_item_container");
+    By articleAboutWowLocator = By.xpath("//*[@text='" + searchWowLine + "']");
+    By articleAboutWotLocator = By.xpath("//*[@text='" + searchWotLine + "']");
+    By pageTitleLocator = By.id("org.wikipedia:id/view_page_title_text");
+
+    waitForNumberOfElementsToBeMoreThan(
+            searchResultLocator,
+            0,
+            String.format("Внимание! В папке '%s' не найдено ни одной статьи.", folderName),
+            15
+    );
+
+    int amountOfArticlesBefore = getAmountOfElements(searchResultLocator);
+
+    Assert.assertEquals(
+            String.format("\nВнимание! В папке '%s' отображается некорректное количество статей.", folderName),
+            amountOfArticlesBefore,
+            2);
+
+    swipeElementToLeft(
+            articleAboutWowLocator,
+            String.format("Внимание! В списке статья с заголовком '%s' не найдена.", searchWowLine)
+    );
+
+    waitForElementNotPresent(
+            articleAboutWowLocator,
+            String.format("Внимание! Статья '%s' не удалилась из списка.", articleAboutWowLocator),
+            15
+    );
+
+    int amountOfArticlesAfter = getAmountOfElements(searchResultLocator);
+
+    Assert.assertEquals(
+            String.format("\nВнимание! В папке '%s' отображается некорректное количество статей.", folderName),
+            amountOfArticlesBefore - 1,
+            amountOfArticlesAfter
+    );
+
+    waitForElementClickableAndClick(
+            articleAboutWotLocator,
+            String.format("Внимание! В папке '%s' статья с заголовком '%s' не найдена или недоступна для действий.",
+                    folderName, searchWotLine),
+            15
+    );
+
+    String actualTitle = waitForElementAndGetAttribute(
+            pageTitleLocator,
+            "text",
+            "Внимание! Название статьи не найдено.",
+            15
+    );
+
+    Assert.assertEquals(
+            "\nВнимание! Отображается некорректное название статьи.",
+            searchWotLine,
+            actualTitle
+    );
+  }
+
   private void assertElementHasText(By by, String expected, String errorMessage) {
     String actual = driver.findElement(by).getAttribute("text");
     Assert.assertEquals(errorMessage, expected, actual);
@@ -569,12 +656,24 @@ public class FirstTest {
     return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
   }
 
+  private WebElement waitForElementClickable(By by, String errorMessage, long timeoutInSeconds) {
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage("\n  " + errorMessage + "\n");
+    return wait.until(ExpectedConditions.elementToBeClickable(by));
+  }
+
   private WebElement waitForElementPresent(By by, String errorMessage) {
     return waitForElementPresent(by, errorMessage, 5);
   }
 
   private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
     WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+    element.click();
+    return element;
+  }
+
+  private WebElement waitForElementClickableAndClick(By by, String errorMessage, long timeoutInSeconds) {
+    WebElement element = waitForElementClickable(by, errorMessage, timeoutInSeconds);
     element.click();
     return element;
   }
@@ -675,5 +774,236 @@ public class FirstTest {
   private String waitForElementAndGetAttribute(By by, String attribute, String errorMessage, long timeoutInSec) {
     WebElement element = waitForElementPresent(by, errorMessage, timeoutInSec);
     return element.getAttribute(attribute);
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private void searchFor(String searchLine) {
+
+    By searchWikipediaLocator = By.xpath("//android.widget.ImageView[@content-desc='Search Wikipedia']");
+    By menuPageSearchLocator = By.id("org.wikipedia:id/menu_page_search");
+    By searchTextLocator = By.id("org.wikipedia:id/search_src_text");
+
+    if (isElementPresent(searchWikipediaLocator)) waitForElementClickableAndClick(
+            searchWikipediaLocator,
+            "Внимание! На главной странице элемент 'Search Wikipedia' не найден или недоступен для действий.",
+            5
+    );
+    else waitForElementClickableAndClick(
+            menuPageSearchLocator,
+            "Внимание! На странице статьи элемент 'Search Wikipedia' не найден или недоступен для действий.",
+            5
+    );
+
+    waitForElementAndSendKeys(
+            searchTextLocator,
+            searchLine,
+            "Внимание! Поле ввода текста для поиска не найдено.",
+            5
+    );
+  }
+
+  private void openArticle(String articleTitle) {
+
+    By resultItemLocator = By.id("org.wikipedia:id/page_list_item_container");
+    By resultTitleLocator = By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + articleTitle + "']");
+    By pageTitleLocator = By.id("org.wikipedia:id/view_page_title_text");
+
+    waitForNumberOfElementsToBeMoreThan(
+            resultItemLocator,
+            0,
+            "Внимание! В результатах поиска ни одной статьи не найдено.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            resultTitleLocator,
+            String.format("Внимание! Статья с заголовком '%s' не найдена или недоступна для действий.", articleTitle),
+            5
+    );
+
+    waitForElementPresent(
+            pageTitleLocator,
+            String.format("Внимание! Заголовок статьи '%s' не найден.", articleTitle),
+            15
+    );
+  }
+
+  private void selectOption(String option) {
+
+    By pageToolbarLocator = By.id("org.wikipedia:id/page_toolbar");
+    By moreOptionsButtonLocator = By.xpath("//android.widget.ImageView[@content-desc='More options']");
+    By optionListLocator = By.xpath("//android.widget.ListView");
+    By optionLocator = By.xpath("//*[@resource-id='org.wikipedia:id/title'][@text='" + option + "']");
+
+    waitForElementVisible(
+            pageToolbarLocator,
+            "Внимание! На странице верхняя панель инструментов не найдена.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            moreOptionsButtonLocator,
+            "Внимание! Кнопка открытия меню действий со статьёй не найдена или недоступна для действий.",
+            5
+    );
+
+    waitForElementVisible(
+            optionListLocator,
+            "Внимание! Меню действий со статьёй не найдено.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            optionLocator,
+            String.format("Внимание! В меню действий со статьёй пция '%s' не найдена или недоступна для действий.", option),
+            5
+    );
+  }
+
+  private void createAndAddArticleTo(String folderName) {
+
+    By bottomSheetLocator = By.id("org.wikipedia:id/design_bottom_sheet");
+    By onboardingButtonLocator = By.id("org.wikipedia:id/onboarding_button");
+    By createButtonLocator = By.id("org.wikipedia:id/create_button");
+    By parentPanelLocator = By.id("org.wikipedia:id/parentPanel");
+    By textInputLocator = By.id("org.wikipedia:id/text_input");
+    By acceptButtonLocator = By.xpath("//*[@resource-id='org.wikipedia:id/buttonPanel']//*[@text='OK']");
+
+    waitForElementVisible(
+            bottomSheetLocator,
+            "Внимание! Элемент 'Bottom Sheet' не найден.",
+            15
+    );
+
+    if (isElementPresent(onboardingButtonLocator)) waitForElementClickableAndClick(
+            onboardingButtonLocator,
+            "Внимание! Кнопка 'GOT IT' не найдена или недоступна для действий.",
+            5
+    );
+    else waitForElementClickableAndClick(
+            createButtonLocator,
+            "Внимание! Кнопка 'Create' не найдена или недоступна для действий.",
+            5
+    );
+
+    waitForElementVisible(
+            parentPanelLocator,
+            "Внимание! Форма создания новой папки для добавления сстатьи не найдена.",
+            15
+    );
+
+    waitForElementAndClear(
+            textInputLocator,
+            "Внимание! Поле ввода имени новой папки для добавления статьи не найдено.",
+            5
+    );
+
+    waitForElementAndSendKeys(
+            textInputLocator,
+            folderName,
+            "Внимание! Невозможно ввести текст в поле ввода имени папки для добавления статьи.",
+            5
+    );
+
+    waitForElementClickableAndClick(
+            acceptButtonLocator,
+            "Внимание! Кнопка 'OK' не найдена или недоступна для действий.",
+            5
+    );
+  }
+
+  private void addArticleTo(String folderName) {
+
+    By bottomSheetLocator = By.id("org.wikipedia:id/design_bottom_sheet");
+    By folderByNameLocator = By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='" + folderName + "']");
+
+    waitForElementVisible(
+            bottomSheetLocator,
+            "Внимание! Элемент 'Bottom Sheet' не найден.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            folderByNameLocator,
+            String.format("Внимание! Папка с именем '%s' не найдена или недоступна для действий.", folderName),
+            5
+    );
+  }
+
+  private void closeArticle() {
+
+    By pageToolbarLocator = By.id("org.wikipedia:id/page_toolbar");
+    By navigateUpButtonLocator = By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']");
+
+    waitForElementVisible(
+            pageToolbarLocator,
+            "Внимание! На странице панель инструментов не найдена.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            navigateUpButtonLocator,
+            "Внимание! Кнопка закрытия статьи не найдена или недоступна для действий.",
+            5
+    );
+  }
+
+  private void openMyLists() {
+
+    By mainNavTabLocator = By.id("org.wikipedia:id/fragment_main_nav_tab_layout");
+    By openMyListsButtonLocator = By.xpath("//android.widget.FrameLayout[@content-desc='My lists']");
+    By myListsPageTitleLocator = By.xpath("//*[@resource-id='org.wikipedia:id/single_fragment_toolbar']/*[@text='My lists']");
+
+    waitForElementVisible(
+            mainNavTabLocator,
+            "Внимание! Главная пнель навигации не найдена.",
+            15
+    );
+
+    waitForElementClickableAndClick(
+            openMyListsButtonLocator,
+            "Внимание! Кнопка перехода к спискам не найдена или недоступна для действий.",
+            5
+    );
+
+    waitForElementPresent(
+            myListsPageTitleLocator,
+            "Внимание! Переход к списку личных папок со статьями не был выполнен.",
+            15
+    );
+  }
+
+  private void openFolder(String folderName) {
+
+    By itemContainerLocator = By.id("org.wikipedia:id/item_container");
+    By folderNameLocator = By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='" + folderName + "']");
+
+    waitForNumberOfElementsToBeMoreThan(
+            itemContainerLocator,
+            0,
+            "Внимание! В списке 'My lists' ни одной папки не найдено.",
+            15
+    );
+
+
+    waitForElementClickableAndClick(
+            folderNameLocator,
+            String.format("Внимание! Папка '%s' не найдена.", folderName),
+            5
+    );
+
+    waitForElementVisible(
+            folderNameLocator,
+            String.format("Внимание! Заголовок папки '%s' не найден.", folderName),
+            15
+    );
   }
 }
