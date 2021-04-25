7af1e9eba03c1557b72139c0cdea63b7cdb7192a
Vasilisa_Smirnova <vssmirnova@vtb.ru>
2021-04-25 14:06:07 +0300

    Создан класс CoreTestCase для инициализации запуска и остановки автотестов
    Lesson: 01. CoreTestCase
    Session: V. Паттерны PageObject, PageFactory и другие

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,50 +1,17 @@
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-
-  private AndroidDriver<?> driver;
-
-  @Before
-  public void SetUp() throws Exception {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("platformName", "Android");
-    capabilities.setCapability("deviceName", "AndroidTestDevice");
-    capabilities.setCapability("platformVersion", "8.0");
-    capabilities.setCapability("automationName", "Appium");
-    capabilities.setCapability("appPackage", "org.wikipedia");
-    capabilities.setCapability("appActivity", ".main.MainActivity");
-    capabilities.setCapability("orientation", "PORTRAIT");
-
-    String oldWikiPath = "./apks/org.wikipedia.old.apk";
-    capabilities.setCapability("app", new File(oldWikiPath).getCanonicalPath());
-/*        String stableWikiPath = "./apks/org.wikipedia.stable.apk";
-        capabilities.setCapability("app", new File(stableWikiPath).getCanonicalPath());*/
-/*        String betaWikiPath = "./apks/org.wikipedia.beta.apk";
-        capabilities.setCapability("app", new File(betaWikiPath).getCanonicalPath());*/
-
-    driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-  }
-
-  @After
-  public void TearDown() {
-    driver.quit();
-  }
+public class FirstTest extends CoreTestCase {
 
   @Test
-  public void testWikiSearch() {
+  public void testSearch() {
 
     waitForElementAndClick(
             By.xpath("//*[contains(@text,'Search Wikipedia')]"),
